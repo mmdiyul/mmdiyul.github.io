ebb7cc6e157e385069fc92bf24466ed29b142a01
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2018-10-19 21:45:49 +0700

    initial commit :panda_face:

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Navbar scroll
+$(document).ready(function() {
+	$(window).scroll(function() {
+        if($(document).scrollTop() > 200) {
+            $('#navbar').addClass('scroll');
+            $('#navbar').addClass('navbar-light');
+            $('#navbar').addClass('bg-white');
+            $('#navbar').addClass('shadow');
+            $('#navbar').removeClass('navbar-dark');
+        }
+        else {
+            $('#navbar').removeClass('scroll');
+            $('#navbar').removeClass('navbar-light');
+            $('#navbar').removeClass('bg-white');
+            $('#navbar').removeClass('shadow');
+            $('#navbar').addClass('navbar-dark');
+        }
+    });
+    
+    // active class
+    $(window).scroll(function () {
+        var currentScrollPos = $(document).scrollTop();
+
+        $('.navbar-nav > .nav-item > a').each(function () {
+            var curLink = $(this);
+            var refElem = $(curLink.attr('href'));
+            if (refElem.position().top <= currentScrollPos && refElem.position().top + refElem.height() > currentScrollPos) {
+                $('.navbar-nav > .nav-item > a').removeClass("active");
+                curLink.parent().addClass("active");
+            }
+            else {
+                curLink.parent().removeClass("active");
+            }
+        });
+    });
+});
+
+// Smooth scroll
+$(document).ready(function() {
+    $('a[href*="#"]').click(function(e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 800, 'swing');
+    });
+});
