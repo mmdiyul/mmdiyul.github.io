605cedfd7a7cc20c18ccb237a0fb046f7138ee34
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2018-10-31 08:59:03 +0700

    delete smooth scroll :panda_face:

--- js/script.js
+++ js/script.js
@@ -33,13 +33,4 @@ $(document).ready(function() {
             }
         });
     });
-
-    // Smooth scroll
-    $('a[href*="#"]').click(function(e) {
-        e.preventDefault();
-
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 800, 'swing');
-    });
 });
